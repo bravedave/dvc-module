00c58e915630abd67418ddec1af9a1f952e95ace
Dave Bray <david@brayworth.com.au>
2019-07-12 12:55:03 +1000

    dev

---
+++ module/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	David Bray
+	BrayWorth Pty Ltd
+	e. david@brayworth.com.au
+
+	This work is licensed under a Creative Commons Attribution 4.0 International Public License.
+		http://creativecommons.org/licenses/by/4.0/
+
+	*/
+namespace dvc\module;
+
+abstract class config extends \config {
+	static $WEBNAME = 'Module Template for DVC';
+
+}
+
--- module/controller.php
+++ module/controller.php
@@ -6,6 +6,8 @@
  * 
  * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
  * 		http://creativecommons.org/licenses/by/4.0/
+ * 
+ * 	http://localhost
  *
  * */
 
@@ -28,6 +30,12 @@ class controller extends \Controller {
 			return ( $view);
 
 		return parent::getView( $viewName, $controller);
+
+	}
+
+	function __construct( $rootPath ) {
+		$this->label = config::$WEBNAME;
+		parent::__construct( $rootPath);
 
 	}
 
