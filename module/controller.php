a45b0d21c619c9389482b863d381b464619c338f
Dave Bray <david@brayworth.com.au>
2019-07-12 13:38:58 +1000

    Update controller.php

--- module/controller.php
+++ module/controller.php
@@ -7,8 +7,6 @@
  * This work is licensed under a Creative Commons Attribution 4.0 International Public License.
  * 		http://creativecommons.org/licenses/by/4.0/
  * 
- * 	http://localhost
- *
  * */
 
 namespace dvc\module;
